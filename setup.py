8dbeee5f226fcb7d29452291041dbff3a3cb847e
jfp1992 <jfp1992@live.co.uk>
2022-10-20 15:38:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="IntegrationCore",
+    name="Playwright-Test-Tooling",
     version="2.0",
     author="James Potter",
     install_requires=REQUIREMENTS,
